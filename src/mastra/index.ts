382006bea945822be3f6330d6222da65b8319cab
jude <matisowagm@gmail.com>
2025-06-19 15:28:50 +0200

    test

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,13 +1,19 @@
-import { Mastra } from '@mastra/core/mastra';
-import { PinoLogger } from '@mastra/loggers';
-import { weatherWorkflow } from './workflows';
-import { weatherAgent } from './agents';
+import { Mastra } from "@mastra/core/mastra";
+import { weatherWorkflow } from "./workflows";
+import { weatherAgent } from "./agents";
+import { weatherTool } from "./tools";
+import { MCPServer } from "@mastra/mcp";
+
+const mcpServer = new MCPServer({
+  name: "My Custom Server",
+  version: "1.0.0",
+  tools: { weatherTool  },
+  agents: { weatherAgent },
+  workflows: { weatherWorkflow },
+});
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
-  logger: new PinoLogger({
-    name: 'Mastra',
-    level: 'info',
-  }),
+  mcpServers: { mcpServer },
 });
